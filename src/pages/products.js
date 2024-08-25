451b3448093836e8120e82a14a70012bb4fe7ad0
zhumingyi94 <dominhnhat2004@gmail.com>
2024-08-25 13:23:26 +0700

    feat: add AIRetrieval Button

--- src/pages/products.js
+++ src/pages/products.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
 import WhiteFooter from '@/components/layout/Footer';
 import NavbarAuth from '@/components/layout/Navbar';
 import FilterSection from '@/components/ui/FilterSection';
@@ -6,7 +8,7 @@ import ProductGrid from '@/components/layout/ProductGrid';
 
 const ProductPage = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-black">
+    <div className="flex flex-col min-h-screen bg-black relative">
       <NavbarAuth />
       <main className="flex-grow flex">
         <aside className="w-[341px] flex-shrink-0">
@@ -17,6 +19,17 @@ const ProductPage = () => {
         </section>
       </main>
       <WhiteFooter />
+      <Link href="/ai-retrieval" className="group">
+        <div className="fixed right-[50px] bottom-[100px] z-50 w-[100px] h-[100px] rounded-full bg-black overflow-hidden shadow-[0px_0px_27.1px_rgba(255,255,255,0.50)] transition-opacity duration-300 ease-in-out group-hover:opacity-70">
+          <Image
+            src="/AIRetrieval.png"
+            alt="AI Retrieval"
+            width={70}
+            height={70}
+            className="absolute pt-[95px] left-1/2 transform -translate-x-1/2 -translate-y-1/2"
+          />
+        </div>
+      </Link>
     </div>
   );
 };
